d605fa340e2a9c3979a110a75acde9c917e78842
SeiKasahara <24386873@student.uwa.edu.au>
2024-05-29 16:11:06 +0800

    Site updated: 2024-05-29 16:11:05

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,14 +1,14 @@
- var OriginTitle = document.title;
+var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/images/1.png");
-        document.title = '╭(°A°`)╮ 勃艮第人……';
+        document.title = '╭(°A°`)╮ No';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
-        document.title = '(ฅ>ω<*ฅ) 欢迎回来达瓦里希' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ) Welcome to ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
